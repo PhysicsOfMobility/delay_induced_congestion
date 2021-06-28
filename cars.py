d818dd224bd8eb897667d668735fc03015d19388
Verena <verena.krall@tu-dresden.de>
2021-06-28 14:04:43 +0200

    Introduced docstrings, adapted some comments

--- cars.py
+++ cars.py
@@ -9,15 +9,27 @@ class Car:
         env,
         start: Tuple[int, int],
         end: Tuple[int, int],
-        delay=0,
+        delay=0.0,
         traffic_info=True,
-        beta=1,
+        beta=1.0,
     ):
+        
+        """ Initiate a car object in the simulation environment.
+        
+        Keyword arguments:
+        start -- origin of the driver (as a tuple giving the node position)
+        end -- destination of the driver (as a tuple giving the node position)
+        delay -- information delay (default 0.0)
+        traffic_info -- whether or not the driver decides based on signalled information (default True)
+        beta -- parameter governing decision making in multinomial logit model (default 1.0)
+        """
+        
         self.env = env
         self.start = start
         self.end = end
-        self.delay = delay  # how old is the traffic information the cars base their choice on (if they use it)
-        # does the car use traffic information at all to choose their path?
+        # How old is the traffic information the cars base their choice on (if they use it)?
+        self.delay = delay  
+        # Does the car use traffic information at all to choose their path?
         self.traffic_info = traffic_info
         self.beta = beta
         self.action = env.process(self.run())
@@ -26,11 +38,14 @@ class Car:
         self.expected_time = 0
 
     def run(self):
+        """ Choose a path from origin to destination, and travel on it. """
         paths = self.env.network.shortestpaths(self.start, self.end)  # possible paths to consider
         times = np.array([self.env.network.path_time(path) for path in paths])
-        times_corrected = times - min(times)  # substract a constant value from all times. This doesn't affect the
-        # outcome because this results in a constant factor for the  probabilities which will be normalised
-        # This helps avoid division by zero if p gets to small
+        # Subtract a constant value from all times. This doesn't affect the
+        # outcome because this results in a constant factor for the  probabilities 
+        # which will be normalised.
+        # This helps avoid division by zero if p gets too small
+        times_corrected = times - min(times)  
         if self.traffic_info:
             p = np.exp(-1 * self.beta * times_corrected)
         else:
@@ -39,14 +54,13 @@ class Car:
         p = p / np.sum(p)
         path_index = np.random.choice(range(len(paths)), p=p)
         path = paths[path_index]
-        self.expected_time = times[path_index]  # calculate time the car expects to take
+        self.expected_time = times[path_index]  # calculate time the car expects to need
         self.starttime = (
             self.env.now + self.delay
         )  # moved in front of delay, to catch errors when accessing the parameter
         self.path = path
         # now wait for a delay before departing
         yield self.env.timeout(self.delay)
-        time = self.env.network.path_time(path)
         self.real_time = 0  # the actual time it will take to traverse the path
         for street in path:
             wait = street.t()
@@ -58,7 +72,7 @@ class Car:
 
 
 class DummyCar:
-    # a dummy class for storing the attributes associated with the car
+    """A dummy class for storing the attributes associated with the car. """
     def __init__(self, car: Car):
         self.start, self.end, self.delay, self.traffic_info = (
             car.start,
