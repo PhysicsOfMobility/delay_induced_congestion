dae05a87273daaa5340c0653cb7bb8130a5c23bf
Verena <verena.krall@tu-dresden.de>
2021-10-11 09:34:41 +0200

    Deleted unnecessary files and removed unnecessary parts of the code.

--- congestion_params.py
+++ congestion_params.py
@@ -228,15 +228,3 @@ def compute_congested(pickledir, outcomefn):
         writer.writerow(["r", "delay", "repetition", "avgtime", "congested"])
         writer.writerows(outcomes)
 
-
-# run_sims(
-#     until=400,
-#     rs=np.arange(184, 202, 2),
-#     delays=[15],
-#     fs=[1],
-#     pointlist=None,
-#     repetitions=100,
-#     pickledir=None,
-#     outcomefn="periodicgrid_congestion_params_rep100_tmax400_r184_202_dr2_f0_10_tau15.csv",
-#     n_jobs=-2,
-# )
